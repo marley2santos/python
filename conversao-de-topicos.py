728a1e50716083e3cf2967a6721627b5a8044d55
marleysanctus <72535842+marleysanctus@users.noreply.github.com>
2022-09-24 17:29:42 -0400

    up

---
+++ conversao-de-topicos.py
@@ -0,0 +1,13 @@
+idade = "26"
+numero1 = "10"
+numero2 = "20"
+
+print(idade, type(idade))
+
+idade_inteira = int(idade)
+
+print(idade_inteira, type(idade_inteira))
+
+altura = float( input("informe sua altura? "))
+
+print(altura, type(altura))
