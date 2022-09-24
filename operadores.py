20597d0e6cca08b15710451b40002adbf722b60e
marleysanctus <72535842+marleysanctus@users.noreply.github.com>
2022-09-23 20:47:18 -0400

    Att

---
+++ operadores.py
@@ -0,0 +1,23 @@
+numero1 = 10
+numero2 = 20
+
+print(numero1 + numero2)
+print(numero1 - numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+print(numero1 // numero2)
+print(20 % 3)
+print(2 ** 3)
+
+idade1 = 10
+idade2 = 15
+altura1 = 1.77
+altura2 = 1.65
+altura3 = altura2
+
+print(idade1 > idade2)
+print(idade1 <= idade2)
+print("Python" == "python")
+print("banana" != "abacaxi")
+print(altura1 >= altura2)
+print(altura2 > altura3)
