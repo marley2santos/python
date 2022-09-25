1d417ddd3a4438d451e2a07d5f605363cb118f1c
marleysanctus <72535842+marley2santos@users.noreply.github.com>
2022-09-25 13:50:36 -0400

    up

---
+++ listas.py
@@ -0,0 +1,27 @@
+notas = [7.9, 9.7, 8.2]
+
+lista = []
+lista = list()
+lista = [26, "Marley", 3.1415, False]
+lista_de_listas = [10, [1, 2, 3]]
+
+lista = [10, "Marley", 3.1415, True]
+
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+
+lista = [10, 50, 30, 40, 25, 60, 5]
+
+print(lista[0:3])
+print(lista[3:6])
+
+for elemento in lista:
+    print(elemento)
+
+print("Comprimento: ", len(lista))
+
+for i in range(len(lista)):
+    print(i)
+    
