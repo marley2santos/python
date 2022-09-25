bf16c9e3a34c5916774d4f33deccf0cf444c8d36
marleysanctus <72535842+marley2santos@users.noreply.github.com>
2022-09-25 14:57:54 -0400

    up

---
+++ dicionarios.py
@@ -0,0 +1,11 @@
+dicionario = {}
+dicionario = dict()
+
+dicionario = {"nome": "Marley", "Idade": 26, "Altura": 1.77}
+
+print(dicionario)
+print(dicionario["Idade"])
+
+dicionario["Programador"] = True
+
+print(dicionario)
