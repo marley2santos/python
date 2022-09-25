38432aa3fb46cd11ef1dbc5ab6f776c632807cbb
marleysanctus <72535842+marley2santos@users.noreply.github.com>
2022-09-25 12:25:02 -0400

    up

---
+++ Lacos_Repeticao.py
@@ -0,0 +1,17 @@
+'''for variavel in range(7):
+    print(variavel)'''
+
+'''for variavel in range(1, 11):
+    print(variavel)'''
+
+'''for variavel in range(1, 14, 3):
+    print(variavel)'''
+
+soma = 0 
+
+for i in range(1, 4):
+    nota = float(input(f"Informe a sua nota{i}: "))
+    
+    soma = soma + nota
+
+print(soma / 3)
